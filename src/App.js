9959038d330b9ff3d4104199ed20b200f25f8082
ori mazrafi <orimazrafi@gmail.com>
2019-04-24 21:58:18 +0300

    conditional rendering in the weather C and displaying error message for user empty input

--- src/App.js
+++ src/App.js
@@ -19,21 +19,32 @@ class App extends Component {
     e.preventDefault();
     const city = e.currentTarget.elements.city.value;
     const country = e.currentTarget.elements.country.value;
-    try {
-      const apiCall = await fetch(
-        `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
-      );
-      const { main, sys, name, weather } = await apiCall.json();
+    if (city && country) {
+      try {
+        const apiCall = await fetch(
+          `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+        );
+        const { main, sys, name, weather } = await apiCall.json();
+        this.setState({
+          temperature: main.temp,
+          city: name,
+          country: sys.country,
+          humidity: main.humidity,
+          description: weather[0].description,
+          error: ''
+        });
+      } catch (ex) {
+        console.log(ex.message);
+      }
+    } else {
       this.setState({
-        temperature: main.temp,
-        city: name,
-        country: sys.country,
-        humidity: main.humidity,
-        description: weather[0].description,
-        error: ''
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'please enter a valid values'
       });
-    } catch (ex) {
-      console.log(ex.message);
     }
   };
   render() {
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -4,10 +4,15 @@ class Weather extends Component {
   render() {
     return (
       <div>
-        Location: {this.props.city}, {this.props.country}
-        Temperature: {this.props.temperature}
-        Humidity: {this.props.humidity}
-        Condition: {this.props.description}
+        {this.props.city && this.props.country && (
+          <p>
+            Location: {this.props.city}, {this.props.country}
+          </p>
+        )}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Condition: {this.props.description}</p>}
+        {this.props.error && <p>{this.props.error}</p>}
       </div>
     );
   }
